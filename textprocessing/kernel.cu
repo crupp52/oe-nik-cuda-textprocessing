9450c838240846e91e6a8d4acac2a81526444627
Zuck Levente <zuck.levente@gmail.com>
2019-11-30 13:46:52 +0100

    Text read

--- textprocessing/kernel.cu
+++ textprocessing/kernel.cu
@@ -3,8 +3,16 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
 
 int main()
 {
-    
+	std::ifstream ifs("text.txt");
+	std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+
+	std::cout << content;
+
+	std::cin.ignore();
+	return 0;
 }
